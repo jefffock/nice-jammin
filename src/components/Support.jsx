80fcf8a017a40c97d16682c33260479b1e5deeaa
Jeff Fox <jeffaveryfox@gmail.com>
2022-02-02 12:09:21 -0600

    Updated support page

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -3,12 +3,20 @@ function Support (props) {
   return (
     <div className="support-container">
       <div className="support-wrapper">
-      <h3>Support</h3>
-      <p>Enjoying Nice Jammin? There are several ways you can show your support:</p>
-      <br></br>
-      <p>1. Sharing your knowledge by adding songs, versions, ratings, comments, and ideas.</p>
-      <br></br>
-      <p>2. Contributing code to the <a href="https://github.com/jefffock/nice-jammin">Github repository</a></p>
+        <h3>Support</h3>
+        <p>Enjoying Nice Jammin? There are several ways you can show your support:</p>
+        <br></br>
+        <div className="support-list-item">
+        <p className="support-list-item">1. Sharing your knowledge by adding songs, versions, ratings, comments, and ideas.</p>
+        <br></br>
+        <p className="support-list-item">2. Sharing it with your friends.</p>
+        <br></br>
+        <p className="support-list-item">3. Contributing code to the <a href="https://github.com/jefffock/nice-jammin">Github repository</a></p>
+        <br></br>
+        <p className="support-list-item">4. If you feel called to support financially, you can do so through <a href="https://www.paypal.com/donate/?business=J23RPKN828CTA&no_recurring=0&item_name=Thank+you+for+supporting+Nice+Jammin%21&currency_code=USD">PayPal</a> or Venmo: @Jeff-Fox</p>
+        </div>
+        <br></br>
+        <p>I see this site as a public good for the jam band community and will never run ads.<br></br><br></br>All support is appreciated. Thank you!</p>
       </div>
     </div>
 
